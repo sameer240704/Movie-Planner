13d331c937ef9fba97af5dcae7ef0c16cd1750f7
sameer240704 <sameergupta79711@gmail.com>
2023-12-28 16:33:25 +0530

    Setup path for files

--- public/src/App.js
+++ public/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/register";
+import Login from "./pages/login";
+import NexVista from "./pages/nexvista";
+import Animation from "./pages/animation";
+import axios from "axios";
+import "./App.css";
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/register" element={<Register />} ></Route>
+        <Route exact path="/login" element={<Login />} ></Route>
+        <Route exact path="/" element={<Animation />} ></Route>
+        <Route exact path="/default" element={<NexVista />} ></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
